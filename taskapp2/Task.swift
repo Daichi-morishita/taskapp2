a5c783738538ee2ef04e90bf7ad0119f622100d1
Daichiーmorishita <wankoro.showman100@gmail.com>
2020-08-26 23:00:05 +0900

    テキスト通り

---
+++ taskapp2/Task.swift
@@ -0,0 +1,28 @@
+//
+//  Task.swift
+//  taskapp2
+//
+//  Created by 森下大地 on 2020/08/26.
+//  Copyright © 2020 daichi.morishita. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
